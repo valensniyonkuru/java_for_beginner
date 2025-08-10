c5f7bda51b74ef1c0aa21c267146726c688dbeb5
Valens NIYONKURU <int0000104@revenue.local>
2025-08-10 12:01:42 +0200

    study getter&setter course

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-     student studentRecord = new student("","auca","IT");
+     student studentRecord = new student("Niyonkuru Valens","auca","IT");
         System.out.println(studentRecord);
+
+// accessing individual field using getter
+        System.out.println("studentName:"+studentRecord.getName());
+        System.out.println("shollName:"+studentRecord.getShoolName());
+        System.out.println("Department:"+studentRecord.getDepartment());
+        // modify/change name
+        studentRecord.setName("uwase cathy");
+        System.out.println(studentRecord);
+
+
+
 
     }
 }
--- src/student.java
+++ src/student.java
@@ -10,8 +10,35 @@ public class student {
     }
    public student(){
     }
+    /* getter method: they are used to access/read private field of class  outSide of class
+                      in controlled format,known as encapsulation.
+    getter methods  return value  */
+
+ public String getName(){
+     return name;
+ }
+public String getShoolName(){
+     return shoolName;
+}
+public String getDepartment(){
+     return department;
+}
+/*
+setter method they do not return value .
+setter method they are used to write/ MODIFY/CHANGE
+setter method are used to validate field
+*/
+    public void setName(String names){
+        this.name=names;
+    }
+     public void setShoolName(String shoolNames){
+        this.shoolName=shoolNames;
+     }
+     public void setDepartment(String departments){
+        this.department=departments;
+     }
 @Override
-    public String toString(){
-        return "Name:" + name+ ", shoolName:"+shoolName+",department:"+department+",";
+   public String toString(){
+      return "Name:" + name+ ", shoolName:"+shoolName+",department:"+department+",";
     }
 }
